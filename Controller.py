9c042d0f3a81b4053b601abac4d7809935d7f2d4
ForestAPC <acbrixia@gmail.com>
2021-10-13 16:28:35 -0400

    Add files via upload
    changed details in controller

--- Controller.py
+++ Controller.py
@@ -1,6 +1,6 @@
 
 
-def MovementLogic(left_sensor, right_sensor, left_speed, right_speed, threshold):
+def MovementLogic(left_bool, right_bool, left_speed, right_speed, threshold):
     '''
     Decides which wheels should move at what speed
     Inputs: sensor booleans, current speeds, sensor thresholds
@@ -12,31 +12,31 @@ def MovementLogic(left_sensor, right_sensor, left_speed, right_speed, threshold)
     catchup_speed = 130
     stop_speed = 0
 
-    'Case One: Nothing on the line'
-    if left_sensor and right_sensor False:
+    #Case One: Nothing on the line
+    if left_bool and right_bool False:
         left_speed = cruising_speed
         right_speed = cruising_speed
 
     
-    'Case Two: left sensor hits line'
-    if left_sensor True and right_sensor False:
+    #Case Two: left sensor hits line
+    if left_bool True and right_bool False:
         right_speed = catchup_speed
         left_speed = catchup_speed
 
-    'Case Three: right sensor hits line'
-    if left_sensor False and right_sensor True:
+    #Case Three: right sensor hits line
+    if left_bool False and right_bool True:
         left_speed = catchup_speed
         right_speed = slowdown_speed
 
-    'Case Four: both sensors hit line'
-    if left_sensor True and right_sensor True:
+    #Case Four: both sensors hit line
+    if left_bool True and right_bool True:
         right_speed = stop_speed
         left_speed = stop_speed
     
     else:
         left_speed = stop_speed
         right_speed = stop_speed
-        print(f"Edge Case (lb:{left_sensor},rb:{right_sensor}, ls:{left_speed}, rs:{right_speed}) ")
+        print(f"Edge Case (lb:{left_bool},rb:{right_bool}, ls:{left_speed}, rs:{right_speed}) ")
 
-    'Return after looping through all cases'
+    #Return after looping through all cases
     return(left_speed, right_speed)
